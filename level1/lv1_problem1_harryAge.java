d6c00c13537c6d689df1715de14730d38005a312
Ankit Raj <ankitrajkumar7667162310@gmail.com>
2025-01-13 13:02:44 +0530

    harry age added

---
+++ level1/lv1_problem1_harryAge.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class lv1_problem1_harryAge {
+    public static void main(String[] args) {
+        // Define current year and birth year
+        int currentYear = 2024;
+        int birthYear = 2000;
+
+        // Calculate age
+        int age = currentYear - birthYear;
+
+        // Display output
+        System.out.println("Harry's age in 2024 is " + age);
+    }
+}
