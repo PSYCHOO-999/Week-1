3ae8d67e34934cd650afaa8b4e770d150fefa6fa
Ankit Raj <ankitrajkumar7667162310@gmail.com>
2025-01-13 13:06:28 +0530

    KM to Miles

---
+++ level1/lv1_problem3_kmToMiles.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class lv1_problem3_kmToMiles {
+    public static void main(String[] args) {
+        // Distance in kilometers
+        double kilometers = 10.8;
+
+        // Conversion factor
+        double miles = kilometers / 1.6;
+
+        // Display output
+        System.out.println("The distance " + kilometers + " km in miles is " + miles);
+    }
+}
