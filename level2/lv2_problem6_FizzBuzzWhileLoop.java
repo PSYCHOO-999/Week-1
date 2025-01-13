7cba56b2a961d58fade55e70792825f7d6ee9a18
Ankit Raj <ankitrajkumar7667162310@gmail.com>
2025-01-13 15:34:11 +0530

    Fizz Buzz 2

---
+++ level2/lv2_problem6_FizzBuzzWhileLoop.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class lv2_problem6_FizzBuzzWhileLoop {
+    public static void main(String[] args) {
+        // Create a Scanner object to read input
+        Scanner input = new Scanner(System.in);
+
+        // Prompt the user to enter a positive integer
+        System.out.print("Enter a positive integer: ");
+        int number = input.nextInt();
+
+        // FizzBuzz logic using a while loop
+        int i = 1;
+        while (i <= number) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
+            i++;
+        }
+
+        // Close the Scanner resource
+        input.close();
+    }
+}
