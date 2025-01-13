3cedfeae77bb0851a47a79208f182de54ab74d6b
Ankit Raj <ankitrajkumar7667162310@gmail.com>
2025-01-13 13:21:27 +0530

    Basic Calc.

---
+++ level1/lv1_problem11_basicCalculator.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class lv1_problem11_basicCalculator {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        // Input two numbers
+        System.out.print("Enter the first number: ");
+        double number1 = input.nextDouble();
+        System.out.print("Enter the second number: ");
+        double number2 = input.nextDouble();
+
+        // Perform operations
+        double addition = number1 + number2;
+        double subtraction = number1 - number2;
+        double multiplication = number1 * number2;
+        double division = number1 / number2;
+
+        // Display output
+        System.out.println("The addition, subtraction, multiplication, and division value of 2 numbers " 
+                           + number1 + " and " + number2 + " is " 
+                           + addition + ", " + subtraction + ", " + multiplication + ", and " + division);
+
+        input.close();
+    }
+}
