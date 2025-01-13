ead94426bc1075cc69429f506437a26b3fe50ec7
Ankit Raj <ankitrajkumar7667162310@gmail.com>
2025-01-13 15:55:11 +0530

    Calculator

---
+++ level3/lv3_problem6_calculator.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class lv3_problem6_calculator {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        double first = input.nextDouble();
+        System.out.print("Enter second number: ");
+        double second = input.nextDouble();
+        System.out.print("Enter an operator (+, -, *, /): ");
+        String op = input.next();
+
+        switch (op) {
+            case "+":
+                System.out.println("Result: " + (first + second));
+                break;
+            case "-":
+                System.out.println("Result: " + (first - second));
+                break;
+            case "*":
+                System.out.println("Result: " + (first * second));
+                break;
+            case "/":
+                if (second != 0) {
+                    System.out.println("Result: " + (first / second));
+                } else {
+                    System.out.println("Division by zero is not allowed.");
+                }
+                break;
+            default:
+                System.out.println("Invalid Operator.");
+        }
+		input.close();
+	}
+}
