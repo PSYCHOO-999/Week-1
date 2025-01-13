309914a1c1b2ede034c0d453760537bc77d2f91f
Ankit Raj <ankitrajkumar7667162310@gmail.com>
2025-01-13 13:27:33 +0530

    que & rem

---
+++ level2/lv2_problem1_quotientRemainder.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class lv2_problem1_quotientRemainder {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        // Input two numbers
+        System.out.print("Enter the first number: ");
+        int number1 = input.nextInt();
+        System.out.print("Enter the second number: ");
+        int number2 = input.nextInt();
+
+        // Calculate quotient and remainder
+        int quotient = number1 / number2;
+        int remainder = number1 % number2;
+
+        // Display output
+        System.out.println("The Quotient is " + quotient + " and Remainder is " + remainder +
+                           " of two numbers " + number1 + " and " + number2);
+
+        input.close();
+    }
+}
